9ea552adde6bf46f939000240f3705f4da3055bb
bbrtj <bbrtj.pro@gmail.com>
2023-08-12 08:32:16 +0200

    Tests for upcoming functions

--- t/lib/CalcTests.pas
+++ t/lib/CalcTests.pas
@@ -25,6 +25,16 @@ type
 		procedure PowerTest();
 		procedure ModuloTest();
 		procedure LogarithmTest();
+		procedure SqrtTest();
+		procedure RandTest();
+		procedure TrigTest();
+		procedure IntegerDivisionTest();
+		procedure FactorialTest();
+		procedure ExpTest();
+		procedure MinMaxTest();
+		procedure SignTest();
+		procedure AbsTest();
+		procedure RoundTest();
 		procedure VariablesTest();
 	end;
 
@@ -45,6 +55,16 @@ begin
 	Scenario(@self.PowerTest, 'should be able to perform power');
 	Scenario(@self.ModuloTest, 'should be able to perform modulo');
 	Scenario(@self.LogarithmTest, 'should be able to calculate logarithms');
+	Scenario(@self.SqrtTest, 'should calculate square root');
+	Scenario(@self.RandTest, 'should yield random numbers');
+	Scenario(@self.TrigTest, 'should calculate trigonometric functions');
+	Scenario(@self.IntegerDivisionTest, 'should be able to perform integer division');
+	Scenario(@self.FactorialTest, 'should calculate factorial');
+	Scenario(@self.ExpTest, 'should calculate exponent');
+	Scenario(@self.MinMaxTest, 'should calculate min/max');
+	Scenario(@self.SignTest, 'should calculate sign of a variable');
+	Scenario(@self.AbsTest, 'should calculate absolute value');
+	Scenario(@self.RoundTest, 'should be able to perform rounding');
 	Scenario(@self.VariablesTest, 'should be able to use variables');
 end;
 
@@ -130,6 +150,107 @@ begin
 
 	FCalc.ParseString('log 1 + 1, 128 + 128 ');
 	TestWithin(FCalc.GetResult, LogN(2, 256), cSmallPrecision);
+end;
+
+procedure TCalculationsSuite.SqrtTest();
+begin
+	FCalc.ParseString('sqrt 25');
+	TestWithin(FCalc.GetResult, 5, cSmallPrecision);
+
+	FCalc.ParseString('sqrt 10000');
+	TestWithin(FCalc.GetResult, 100, cSmallPrecision);
+end;
+
+procedure TCalculationsSuite.RandTest();
+var
+	vResult: Double;
+begin
+	FCalc.ParseString('rand 15156512');
+	vResult := FCalc.GetResult;
+
+	FCalc.ParseString('rand 15156512');
+	TestOk(FCalc.GetResult <> vResult);
+end;
+
+procedure TCalculationsSuite.TrigTest();
+begin
+	FCalc.ParseString('sin 51.5');
+	TestWithin(FCalc.GetResult, Sin(51.5), cSmallPrecision, 'sinus');
+
+	FCalc.ParseString('cos 51.5');
+	TestWithin(FCalc.GetResult, Cos(51.5), cSmallPrecision, 'cosinus');
+
+	FCalc.ParseString('tan 51.5');
+	TestWithin(FCalc.GetResult, Tan(51.5), cSmallPrecision, 'tangent');
+
+	FCalc.ParseString('cot 51.5');
+	TestWithin(FCalc.GetResult, Cotan(51.5), cSmallPrecision, 'cotangent');
+
+	FCalc.ParseString('arcsin 0.7');
+	TestWithin(FCalc.GetResult, ArcSin(0.7), cSmallPrecision, 'arcus sinus');
+
+	FCalc.ParseString('arccos 0.7');
+	TestWithin(FCalc.GetResult, ArcCos(0.7), cSmallPrecision, 'arcus cosinus');
+end;
+
+procedure TCalculationsSuite.IntegerDivisionTest();
+begin
+	FCalc.ParseString('5120 div 15');
+	TestWithin(FCalc.GetResult, 5120 div 15, cSmallPrecision);
+end;
+
+procedure TCalculationsSuite.FactorialTest();
+begin
+	FCalc.ParseString('fact 10');
+	TestWithin(FCalc.GetResult, 3628800, cSmallPrecision);
+end;
+
+procedure TCalculationsSuite.ExpTest();
+begin
+	FCalc.ParseString('exp 19');
+	TestWithin(FCalc.GetResult, Exp(19), cSmallPrecision, '??');
+end;
+
+procedure TCalculationsSuite.MinMaxTest();
+begin
+	FCalc.ParseString('min 6, 8');
+	TestWithin(FCalc.GetResult, 6, cSmallPrecision, 'min');
+
+	FCalc.ParseString('max 6, 8');
+	TestWithin(FCalc.GetResult, 8, cSmallPrecision, 'max');
+end;
+
+procedure TCalculationsSuite.SignTest();
+begin
+	FCalc.ParseString('sign 156');
+	TestWithin(FCalc.GetResult, 1, cSmallPrecision, 'positive');
+
+	FCalc.ParseString('sign 0');
+	TestWithin(FCalc.GetResult, 0, cSmallPrecision, 'zero');
+
+	FCalc.ParseString('sign -0.0005124');
+	TestWithin(FCalc.GetResult, -1, cSmallPrecision, 'negative');
+end;
+
+procedure TCalculationsSuite.AbsTest();
+begin
+	FCalc.ParseString('abs 15');
+	TestWithin(FCalc.GetResult, 15, cSmallPrecision, 'positive');
+
+	FCalc.ParseString('abs -15');
+	TestWithin(FCalc.GetResult, 15, cSmallPrecision, 'negative');
+end;
+
+procedure TCalculationsSuite.RoundTest();
+begin
+	FCalc.ParseString('round 15.66');
+	TestWithin(FCalc.GetResult, 16, cSmallPrecision, 'round');
+
+	FCalc.ParseString('floor 15.66');
+	TestWithin(FCalc.GetResult, 15, cSmallPrecision, 'floor');
+
+	FCalc.ParseString('ceil 15.66');
+	TestWithin(FCalc.GetResult, 16, cSmallPrecision, 'ceil');
 end;
 
 procedure TCalculationsSuite.VariablesTest();
