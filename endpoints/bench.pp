4eeea2451853a81737d77e51068ca5fc39ce7390
bbrtj <bbrtj.pro@gmail.com>
2024-08-25 18:47:22 +0200

    Use more complex bench scenario

--- endpoints/bench.pp
+++ endpoints/bench.pp
@@ -14,7 +14,7 @@ begin
 	Total := 0;
 	for Variable := 1 to 20 * 30 * 20 do begin
 		Calc := TPN.Create;
-		Calc.ParseString('2 + 3 / 5 * var1 ^ 4 - (8 - 16 * 32 + (51 * 49))');
+		Calc.ParseString('2.81 + 3E5 / 5 * var1 ^ 4 - (8.81 - 16 * 32 + (51 * 49.999))');
 		Calc.DefineVariable('var1', Variable);
 		Total += Calc.GetResult();
 		Calc.Free;
